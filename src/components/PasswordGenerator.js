af9c4eff246378f7fcd16614da125ac4d4971245
RonflexAzOg <lucas.barray@outlook.fr>
2023-09-27 14:04:39 +0200

    create passwordgenerator

---
+++ src/components/PasswordGenerator.js
@@ -0,0 +1,12 @@
+function PasswordGenerator() {
+  return (
+    <div className="App">
+      <form>
+        <input type="checkbox" id="symbol" />
+        <label for="symbol">Include symbols</label>
+      </form>
+    </div>
+  );
+}
+
+export default PasswordGenerator;
