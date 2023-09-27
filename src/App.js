87e57d573b382625ca39f9803de4ca59ef6fb351
RonflexAzOg <lucas.barray@outlook.fr>
2023-09-27 13:51:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Password Generator
         </p>
         <a
           className="App-link"
@@ -15,9 +15,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Generate password
         </a>
       </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
